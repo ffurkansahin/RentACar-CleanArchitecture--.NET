4c4cfbdea69567341099039a18b4e69c53873c91
Furkan Şahin <furkan.sahinbusiness@gmail.com>
2024-04-06 23:21:06 +0300

    first commit

---
+++ src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+
+	public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+	{
+		public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+		{
+			CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+			createdBrandResponse.Name = request.Name;
+			createdBrandResponse.Id = new Guid();
+			return null;
+		}
+	}
+
+}
---
+++ src/rentACar/RentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,22 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Brand : Entity<Guid>
+{
+    public Brand()
+    {
+        
+    }
+    public Brand(Guid id,string name)
+    {
+        Id= id;
+        Name= name;
+    }
+    public string Name { get; set; }
+}
